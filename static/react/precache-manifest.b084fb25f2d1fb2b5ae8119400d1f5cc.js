3f0d9b2630a2c33586f2815d1e53977cb0d70098
Israel Ali <israelali1424@gmail.com>
2020-01-27 00:30:43 -0500

    first commit

---
+++ static/react/precache-manifest.b084fb25f2d1fb2b5ae8119400d1f5cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72578f898b1e08c6c3b46a32896a86ab",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "fa6ca2bc40f0049812f5",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "57841073555efcd729c8",
+    "url": "/static/react/js/2.c9730a39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/react/js/2.c9730a39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa6ca2bc40f0049812f5",
+    "url": "/static/react/js/main.c3e20f02.chunk.js"
+  },
+  {
+    "revision": "1c67bf2225e5a0f28767",
+    "url": "/static/react/js/runtime-main.4e8b2abb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
